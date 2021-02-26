d3f95c15a44c97966a45001df64e30671ab67d08
mjbns-iscte <mjbns@iscte-iul.pt>
2021-02-26 17:18:26 +0000

    Miguel_Par

---
+++ Projeto/src/Miguel.java
@@ -0,0 +1,17 @@
+
+public class Miguel {
+	private int num;
+
+	public Miguel(int num) {
+		this.num = num;
+	}
+	
+	public boolean Par(int num) {
+		if(num%2==0) {
+			return true;
+		}
+		else return false;
+	}
+	
+
+}
